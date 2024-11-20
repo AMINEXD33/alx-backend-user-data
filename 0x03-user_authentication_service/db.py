c513e1a1802f5ec4707b4b2ad80c255c5ac00d73
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-20 18:56:58 +0100

    added some typing to the function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """DB module
 """
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -39,7 +38,7 @@ class DB:
         self._session.flush()
         return tmp
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """
         a function that finds a user with some args
         and returns the  User object, or raise an error if the kwargs
