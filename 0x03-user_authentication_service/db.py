1c85ff49c6f39ee38de69be4e7fd57a98e01d6b5
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-20 18:38:12 +0100

    add_user() function is emplimented

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -27,22 +27,13 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Add new user to database
-        Returns a User object
-        """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
 
-    # def add_user(self, email: str, hashed_password: str) -> User:
-    #     """
-    #     a function that adds a new user to the database
-    #     and returns a User object
-    #     """
-    #     tmp = User(email=email, hashed_password=hashed_password)
-    #     self._session.add(tmp)
-    #     self._session.flush()
-    #     return tmp
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        a function that adds a new user to the database
+        and returns a User object
+        """
+        tmp = User(email=email, hashed_password=hashed_password)
+        self._session.add(tmp)
+        self._session.flush()
+        return tmp
