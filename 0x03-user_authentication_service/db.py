9ac11eefb076ec3ad03f70fab85dc97f746da4ad
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-20 19:08:04 +0100

    style might be a prob

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,7 +8,6 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
-from sqlalchemy import create_engine, tuple_
 
 
 class DB:
@@ -39,45 +38,27 @@ class DB:
         self._session.flush()
         return tmp
 
-    def find_user_by(self, **kwargs) -> User:
-        """Finds a user based on a set of filters."""
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = (
-            self._session.query(User)
-            .filter(tuple_(*fields).in_([tuple(values)]))
-            .first()
+    def find_user_by(self, **kwargs: dict) -> User:
+        """
+        a function that finds a user with some args
+        and returns the  User object, or raise an error if the kwargs
+        are wrong or no User was found
+        """
+        keys: list[str] = [
+            "id",
+            "email",
+            "hashed_password",
+            "session_id",
+            "reset_token",
+        ]
+        # check keys
+        for key in kwargs.keys():
+            if key not in keys:
+                raise InvalidRequestError
+        # try and kind the user with such keys
+        potential_user: User | None = (
+            self._session.query(User).filter_by(**kwargs).first()
         )
-        if result is None:
-            raise NoResultFound()
-        return result
-
-    # def find_user_by(self, **kwargs) -> User:
-    #     """
-    #     a function that finds a user with some args
-    #     and returns the  User object, or raise an error if the kwargs
-    #     are wrong or no User was found
-    #     """
-    #     keys: list[str] = [
-    #         "id",
-    #         "email",
-    #         "hashed_password",
-    #         "session_id",
-    #         "reset_token",
-    #     ]
-    #     # check keys
-    #     for key in kwargs.keys():
-    #         if key not in keys:
-    #             raise InvalidRequestError
-    #     # try and kind the user with such keys
-    #     potential_user: User | None = (
-    #         self._session.query(User).filter_by(**kwargs).first()
-    #     )
-    #     if potential_user is None:
-    #         raise NoResultFound
-    #     return potential_user
+        if potential_user is None:
+            raise NoResultFound
+        return potential_user
