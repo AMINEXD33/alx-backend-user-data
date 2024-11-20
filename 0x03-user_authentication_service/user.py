1311ad1f48ee4510d98fbb78ae41af726a16b124
AMINEXD33 <amine.gamer222@gmail.com>
2024-11-20 19:10:58 +0100

    style might be a prob

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,8 +13,8 @@ class User(Base):
     """
 
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True,  autoincrement=True)
     email = Column(String(length=250), nullable=False)
-    hashed_password = Column(String(length=250), nullable=False)
-    session_id = Column(String(length=250), nullable=False)
-    reset_token = Column(String(length=250), nullable=False)
+    hashed_password = Column(String(length=250), nullable=True)
+    session_id = Column(String(length=250), nullable=True)
+    reset_token = Column(String(length=250), nullable=True)
